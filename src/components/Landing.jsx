ca44b00f522b210352392e0a5ca62b81c905308a
prakash <prakashjaiswal.pj16@gamil.com>
2025-08-23 01:17:43 +0530

    langing page done

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,23 +2,24 @@ import curveBg from"../assets/curveBg.jpg";
 let Landing =()=>{
     return(<>
    
-    <section >
+    <section className="section" >
         {/* write your html here add css in index.css */}
         <div className="landing-container">
             {/* Navbar */}
         <nav className="navbar">
-            <div>Mrs. College Guide</div>
+            <div className="logo">Mrs. College Guide</div>
          <ul className="nav-links">
             <li><a href="#">Universities</a></li>
             <li><a href="#">Courses</a></li>
             <li><a href="#">Community</a></li>
             <li><a href="#">News</a></li>
             <li><a href="#">About</a></li>
-            <curveBg/>
+            
          </ul>
-         <div className="nav-buttons"></div>
+         <div className="nav-buttons">
          <button className="login-btn">LOGIN</button>
          <button className="signup-btn">SIGN UP</button>
+         </div>
         </nav>
         </div>
         {/* Hero Section */}
@@ -36,11 +37,10 @@ let Landing =()=>{
       {/* Info Section */}
       <div className="info-section">
         <p>
-          Explore your options and make informed decisions with our comprehensive guide to 
-          universities around the world. Discover top-ranked institutions, explore diverse programs, 
-          and connect with like-minded individuals to build your academic future. With easy-to-use 
+          Explore your options and make informed decisions with our comprehensive guide to universities around the world. Discover top-ranked institutions, explore diverse programs, and connect with like-minded individuals to build your academic future. With easy-to-use search tools, in-depth profiles, and trusted ratings and reviews, we provide everything you need to make the right choice for your academic journey. Start your search today and find your perfect fit!
           </p>
         </div>
+          <button className="get-start">get Started</button>
     </section>
      
     </>)
