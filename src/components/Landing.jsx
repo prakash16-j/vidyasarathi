13c632ade5a853eef013783a211dbae3eb8d2d26
prakash <prakashjaiswal.pj16@gamil.com>
2025-08-22 23:42:09 +0530

    error solved in landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,11 +1,8 @@
 import curveBg from"../assets/curveBg.jpg";
 let Landing =()=>{
     return(<>
-    <body className="landing-container" style={{backgroundImage: `url(${curveBg})`,
-backgroundRepeat: "no-repeat",
-backgroundPosition: "center bottom",
-backgroundSize:"cover",}}>
-    <section>
+   
+    <section >
         {/* write your html here add css in index.css */}
         <div className="landing-container">
             {/* Navbar */}
@@ -17,12 +14,14 @@ backgroundSize:"cover",}}>
             <li><a href="#">Community</a></li>
             <li><a href="#">News</a></li>
             <li><a href="#">About</a></li>
-            <curveBg/>
+            
          </ul>
+
          <div className="nav-buttons"></div>
          <button className="login-btn">LOGIN</button>
          <button className="signup-btn">SIGN UP</button>
         </nav>
+        </div>
         {/* Hero Section */}
       <section className="hero">
         <div className="hero-overlay">
@@ -36,14 +35,16 @@ backgroundSize:"cover",}}>
       </section>
 
       {/* Info Section */}
-      <section className="info-section">
+      <div className="info-section">
         <p>
           Explore your options and make informed decisions with our comprehensive guide to 
           universities around the world. Discover top-ranked institutions, explore diverse programs, 
           and connect with like-minded individuals to build your academic future. With easy-to-use 
+          </p>
         </div>
+        
     </section>
-     </body>
+     
     </>)
 }
 export default Landing
