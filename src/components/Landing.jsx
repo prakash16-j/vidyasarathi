a8302068d23957105f433738d1f696e62c80ca3c
vaishnavidhatrika17@gmail.com <vaishnavidhatrika17@gmail.com>
2025-08-23 08:30:10 +0530

    Adding local changes

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,4 +1,5 @@
 import curveBg from"../assets/curveBg.jpg";
+import Bgimage from"../assets/Bgimage.jpg";
 let Landing =()=>{
     return(<>
     <body className="landing-container" style={{backgroundImage: `url(${curveBg})`,
@@ -17,7 +18,6 @@ backgroundSize:"cover",}}>
             <li><a href="#">Community</a></li>
             <li><a href="#">News</a></li>
             <li><a href="#">About</a></li>
-            <curveBg/>
          </ul>
          <div className="nav-buttons"></div>
          <button className="login-btn">LOGIN</button>
@@ -27,7 +27,10 @@ backgroundSize:"cover",}}>
         </section>
 
         {/* Hero Section */}
-      <section className="hero">
+      <section className="body" style={{backgroundImage: `url(${Bgimage})`,
+backgroundRepeat: "no-repeat",
+backgroundPosition: "center bottom",
+backgroundSize:"cover",}} >
         <div className="hero-overlay">
           <h1>
             Find Your <span className="highlight">Future</span> Today!
@@ -40,7 +43,7 @@ backgroundSize:"cover",}}>
 
       {/* Info Section */}
       <section className="info-section">
-        <p>
+        <p >
           Explore your options and make informed decisions with our comprehensive guide to 
           universities around the world. Discover top-ranked institutions, explore diverse programs, 
           and connect with like-minded individuals to build your academic future. With easy-to-use  search tools, in-depth profiles, and trusted ratings and reviews, we provide everything 
