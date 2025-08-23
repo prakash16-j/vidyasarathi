080556b6e91c1300327dd44cc8faca51f8217940
ritesh <ritesh@gmail.com>
2025-08-23 20:38:38 +0530

    loginpage

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -1,7 +1,40 @@
 let AdminLogin=()=>{
     return(<>
+    
     <section>
-        {/* write your html here add css in index.css */}
+        <body>
+  <section className="mainheading">
+    <div className="left-content">
+      <h1>VIDYA SARTHI</h1>
+      <h2>Welcome to Vidya Sarthi</h2>
+      <p>
+        A digital bridge between students and teachers, where teachers share 
+        knowledge and students access notes with ease.
+      </p>
+    </div>
+
+    <div className="login-box">
+      <h3>Login</h3>
+      <hr style="width:50%;text-align:left;margin-left:0;margin-top:10px;" color="black" />
+      <p>Welcome onboard with us!</p>
+
+      <form>
+        <label htmlFor="email">Admin Email ID</label>
+        <input className="new" type="email" id="email" placeholder="Enter your username" />
+
+        <label htmlFor="password">Password</label>
+        <input className="new" type="password" id="password" placeholder="Enter your password" />
+
+        <div className="extra">
+          <a href="#">Forgot Password?</a>
+        </div>
+
+        <button className="logbutton" type="submit">LogIn</button>
+      </form>
+    </div>
+  </section>
+</body>
+
     </section>
     </>)
 }
