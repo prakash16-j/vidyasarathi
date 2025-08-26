1845dffd034afec4540a188bfd8e1a1ff7eddc08
vaishnavidhatrika17@gmail.com <vaishnavidhatrika17@gmail.com>
2025-08-26 14:47:49 +0530

    c1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Admin_teacherSection from "./components/Admin_teacherSection"
 import AdminLogin from "./components/AdminLogin"
 import Landing from "./components/Landing"
-import Student from "./components/Student"
 
 function App() {
 
--- src/components/Admin_teacherSection.jsx
+++ src/components/Admin_teacherSection.jsx
@@ -2,7 +2,56 @@ let Admin_teacherSection =()=>{
     return(
        <section className="bg-red-200 h-100vh">
         {/* start code from here */}
-       </section>
+      <div className="container1">
+      <div className="card1">
+        {/* Header */}
+        <div className="header1">
+          <h2 className="header2">Search for teacher:</h2>
+          <label className="radio-label1">
+            <input type="radio" name="master" className="radio1"/>
+            Master List
+          </label>
+        </div>
+
+        <hr className="horizontal1" />
+
+        {/* Search by list */}
+         <div className="section1">
+          <h3>Search by List:</h3>
+          <div className="row1">
+            <select>
+              <option>Select Course</option>
+            </select>
+            <select>
+              <option>Select Batch</option>
+            </select>
+            <select>
+              <option>Select Branch</option>
+            </select>
+            <button className="btn1">Search</button>
+          </div>
+        </div>
+
+         {/* Specific Search */}
+        <div className="section2">
+          <h3>Specific Search:</h3>
+          <div className="row2">
+            {/* <input type="text" placeholder="Enter Roll Number" /> */}
+            <label className="input-label">Enter Roll Number</label>
+            <input type="text" placeholder="Name (Auto)" />
+            <button className="btn2">Apply</button>
+          </div>
+        </div>
+
+        </div>
+        </div>
+      <div className="footer1">
+      <span className="text1">Export Result as excel file.</span>
+      <span className="last-updated">Last updated on: 19 August | 23:00</span>
+      <button className="export-btn">Export</button>
+    </div>
+</section>
+
     )
 
 }
