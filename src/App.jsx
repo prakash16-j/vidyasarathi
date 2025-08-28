f8dc034edcc4afaf023c23672be0cd7b45b04b52
ritesh <ritesh@gmail.com>
2025-08-28 13:34:31 +0530

    specificsearch and contact done

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@
 import Admin_StudentSection from "./components/Admin_StudentSection"
 import Admin_teacherSection from "./components/Admin_teacherSection"
 import AdminLogin from "./components/AdminLogin"
+import Contact from "./components/contact"
 import Landing from "./components/Landing"
 import Login from "./components/Login"
 import Quickaction from "./components/Quick actions"
+import SpecificSearch from "./components/specificsearch_and_contact"
 
 
 import StudentTable from "./components/StudentTable"
 
 function App() {
-
   return (
     <>
      <Landing/>
@@ -18,7 +19,8 @@ function App() {
      <AdminLogin/>
      <Admin_teacherSection/>
      <Admin_StudentSection/>
-  
+  <SpecificSearch/>
+  <Contact></Contact>
     </>
   )
 }
