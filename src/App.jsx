6a07b89943c9825a3d7d9a1870543dc85438a432
Amulya <amulyagoud2004@gmail.com>
2025-08-29 10:27:16 +0530

    amulya commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,19 @@ import Landing from "./components/Landing"
 import Card3 from "./components/Card3"
 import Login from "./components/Login"
 import Quickaction from "./components/Quick actions"
-<<<<<<< HEAD
 import StudentDetails from "./components/StudentDetails"
-=======
 import SpecificSearch from "./components/specificsearch_and_contact"
->>>>>>> bfe904593feea58f20e5e463ead9f05b42003ff8
-
-
 import StudentTable from "./components/StudentTable"
 
 function App() {
   return (
     <>
-     {/* <Landing/>
+     <Landing/>
      <Login/>
      <AdminLogin/>
      <Admin_teacherSection/>
-<<<<<<< HEAD
-     <Admin_StudentSection/> */}
+     <Admin_StudentSection/>
     <StudentDetails/>
-=======
      <Admin_StudentSection/>
 
      <Card3></Card3>
@@ -35,7 +28,6 @@ function App() {
   
   <SpecificSearch/>
   <Contact></Contact>
->>>>>>> bfe904593feea58f20e5e463ead9f05b42003ff8
     </>
   )
 }
