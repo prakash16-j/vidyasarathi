67386124c354d3340c337f6814b242b350b29f3c
vaishnavidhatrika17@gmail.com <vaishnavidhatrika17@gmail.com>
2025-09-01 12:28:52 +0530

    changes in AddRegulation_V

--- src/components/AddRegulation_V.jsx
+++ src/components/AddRegulation_V.jsx
@@ -1,7 +1,7 @@
 let AddRegulation_V = () => {
   return (
     <>
-      <div className="  drop-shadow-2xl  h-80 flex justify-center bg-white">
+      <div className="  drop-shadow-2xl  h-90 flex justify-center bg-white">
         <div className="  flex justify-between ">
         <div className="w-full ml-20 bg-white shadow-md rounded-lg p-8  w-200 bg-[red]">
           {/* Title */}
@@ -9,10 +9,10 @@ let AddRegulation_V = () => {
 
           {/* Regulation */}
           <div className="mb-4 flex items-center w-250   ">
-            <label className="font-medium w-50">Choose Regulation :</label>
-            <select className="w-150  bg-blue-100 border border-gray-100 rounded-lg px-10 py-2">
-              <option className="w-full">
-                Choose a Regulation from drop down
+            <label className="font-medium w-50 ">Choose Regulation :</label>
+            <select className="w-170 h-11 bg-blue-100 border border-gray-100 rounded-lg shadow-lg drop-shadow-2xl p1-4">
+              <option className="w-full  h-30 drop">
+                     Choose a Regulation from drop down
               </option>
             </select>
           </div>
@@ -21,7 +21,7 @@ let AddRegulation_V = () => {
           {/* Branch */}
           <div className="mb-4  flex items-center">
             <label className="w-50 font-medium">Choose Branch :</label>
-            <select className="w-150 bg-blue-100 border border-gray-300 rounded-lg px-4 py-2">
+            <select className="w-170 h-11 bg-blue-100 border border-gray-300 rounded-lg px-4 py-2 shadow-lg drop-shadow-2xl">
               <option className="w-full">Choose a Branch from drop down</option>
             </select>
           </div>
@@ -29,7 +29,7 @@ let AddRegulation_V = () => {
           {/* Semester */}
           <div className="mb-6 flex items-center">
             <label className="w-50 font-medium">Choose Semister :</label>
-            <select className="w-150 bg-blue-100 border border-gray-300 rounded-lg px-4 py-2">
+            <select className="w-170 h-11 bg-blue-100 border border-gray-300 rounded-lg px-4 py-2 shadow-lg drop-shadow-2xl">
               <option className="w-full">Choose a Semister from drop down</option>
             </select>
           </div>
@@ -40,7 +40,8 @@ let AddRegulation_V = () => {
             <label className="font-semibold  mb-3">
               Subjects: <br /> <span className="font-normal">(Choose Priority Wise)</span>
             </label>
-            <div className="flex justify-center  w-170 border-solid   gap-3">
+            <div className=" w-183 flex justify-center shadow-lg drop-shadow-2xl ">
+            <div className="flex justify-center  w-170 border-solid    gap-4">
               {["A", "B", "C", "D", "E", "F"].map((letter) => (
                 <select
                   key={letter}
@@ -49,6 +50,7 @@ let AddRegulation_V = () => {
                   <option className="">{letter}. Choose</option>
                 </select>
               ))}
+            </div>
             </div>
           </div>
 <br />
