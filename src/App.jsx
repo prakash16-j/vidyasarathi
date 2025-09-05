5e2f1145ef8bbf19eb23c7c89411896e545f5d63
ritesh <ritesh@gmail.com>
2025-09-05 13:26:00 +0530

    enacp latest nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@
 import { Outlet } from "react-router-dom";
 import SignUp from "./components/Students/StudentSignUP";
 import StudentSignUP from "./components/Students/StudentSignUP";
+import Latest from "./components/Students/Latest";
+import Encapsulation from "./components/Students/Encapsulation";
 
 function App() {
   return (
     <>
      
       {/* <Outlet /> 👈 Public pages (Landing, Login, Teacher Login, etc.) */}
-      <StudentSignUP></StudentSignUP>
+      {/* <StudentSignUP></StudentSignUP> */}
+       {/* Ritesh task on 5-sept */}
+      {/* <Latest/> */}
+      <Encapsulation></Encapsulation>
     </>
   );
 }
