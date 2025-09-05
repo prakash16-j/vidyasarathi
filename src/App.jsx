44327f597680210489320860732867593791a7bc
ritesh <ritesh@gmail.com>
2025-09-05 11:46:13 +0530

    StudentSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 // import { Outlet } from "react-router-dom";
 
 import { Outlet } from "react-router-dom";
+import SignUp from "./components/Students/StudentSignUP";
+import StudentSignUP from "./components/Students/StudentSignUP";
 
 function App() {
   return (
     <>
      
-      <Outlet /> {/* 👈 Public pages (Landing, Login, Teacher Login, etc.) */}
+      {/* <Outlet /> 👈 Public pages (Landing, Login, Teacher Login, etc.) */}
+      <StudentSignUP></StudentSignUP>
     </>
   );
 }
